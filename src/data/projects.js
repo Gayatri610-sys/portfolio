b096de8a344fc6e1783ae85f8bd90b90f86c5ade
Gayatri610-sys <mgayatrirao610@gmail.com>
2026-05-12 12:46:11 +0530

    updated portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,17 +2,20 @@ const projects=[
     {
         title:"Bank Management System",
         description: "A console-based Java application developed using object-oriented programming concepts.The system allows users to create accounts, deposit and withdraw money, and view transaction details.",
-        tech:"Java,OOP"
+        tech:"Java,OOP",
+        github:"https://github.com/Gayatri610-sys/bank-management-system"
     },
     {
         title: "E-commerce Application",
         description:"Console-based shopping system with login and billing.",
-        tech:"Java,OOP"
+        tech:"Java,OOP",
+        github:"https://github.com/Gayatri610-sys/Ecommerce-project"
     },
     {
         title:"Online Electronic Portal",
         description:"Web-based UI project for product display.",
-        tech:"Java,JDBC,SQL,HTML,CSS,JavaScript"
+        tech:"Java,JDBC,SQL,HTML,CSS,JavaScript",
+        github:"https://github.com/Gayatri610-sys/Online-Electronic-Portal"
     }
 ];
 
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,25 +1,41 @@
 import projects from "../data/projects";
+import { FaGithub } from "react-icons/fa";
 
-function Projects(){
-    return(
-        <section id="projects" className="fade">
-            <div className="container">
-               <h2 style={{ marginBottom: "20px", color: "#38bdf8" }}>
-          Projects
-        </h2>
+function Projects() {
+  return (
+    <section id="projects" className="fade">
+      <div className="container">
+        <h2>Projects</h2>
 
-                <div className="projects-grid">
-                    {projects.map((project,index)=>(
-                        <div className="card" key={index}>
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <p><b>{project.tech}</b></p>
-                    </div>
-                    ))}
+        <div className="projects-grid">
+          {projects.map((project, index) => (
+            <a
+              href={project.github}
+              target="_blank"
+              rel="noreferrer"
+              key={index}
+              className="project-link"
+            >
+              <div className="card">
+                <h3>{project.title}</h3>
+
+                <p>{project.description}</p>
+
+                <span style={{ color: "#38bdf8" }}>
+                  {project.tech}
+                </span>
+
+                <div className="github-section">
+                        <FaGithub className="github-icon"/>
+
                 </div>
-            </div>
-        </section>
-    );
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
